95984d7ab0d2e4fd9140c9fe34064f78aa168d72
madenityan <marinakorsyn@gmail.com>
2018-01-16 01:18:01 +0200

    added adaptation home and about pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,45 @@
 $(document).ready(function() {
 
     $('.slider').slick({
+        slidesToShow:4,
+        slidesToScroll: 1,
         dots: true,
-        infinite: false,
         arrows:false,
-        fade:true,
-        adaptiveHeight: true
-        // autoplay:true,
-        // autoplaySpeed:1500
+        adaptiveHeight: true,
+        autoplay:true,
+        autoplaySpeed:1000,
 
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+
+
+                }
+            }
+        ]
     });
+
+
 
      $('.slider-logo').slick({
         slidesToShow:5,
@@ -63,9 +93,9 @@ $(document).ready(function() {
         infinite: false,
         arrows:false,
         fade:true,
-        adaptiveHeight: true
-        // autoplay:true,
-        // autoplaySpeed:1500
+        adaptiveHeight: true,
+        autoplay:true,
+        autoplaySpeed:1500
 
     });
 
@@ -74,6 +104,16 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).parent().parent().parent().find('.slide-toggle').slideToggle();
     });
+
+
+    var $container = $('.masonry-container');
+        $container.masonry( {
+            columWidht: '.item',
+            itemSelector: '.item',
+            gutter: 35
+        });
+
+
 
 });
 
